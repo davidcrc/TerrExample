74b9b682fb83452332977f39a037dd69f3c66692
davidcrc <david_drcr@hotmail.com>
2018-10-20 10:51:08 -0500

    Ejemplo para correr docker con terrfrm

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Configure Docker provider and connect to the local Docker socket
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:1.11-alpine"
+}
+
+resource "docker_container" "nginx-server" {
+  name  = "nginx-server"
+  image = "${docker_image.nginx.latest}"
+
+  ports {
+    internal = 80
+  }
+
+  volumes {
+    container_path = "/usr/share/nginx/html"
+    host_path      = "/home/scrapbook/tutorial/www"
+    read_only      = true
+  }
+}
